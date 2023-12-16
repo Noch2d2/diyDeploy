d07a5ab08f5a9d7d74d1ab0e0ec3bfc562a1a27d
Anochie Ugochuku <augochuku@indeed.com>
2023-12-15 16:04:40 -0800

    Log environment

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,4 +2,5 @@ const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
 console.log("Using knex configuration for " + environment + " environment");
+console.log(config);
 module.exports = knex;
